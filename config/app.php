1ae6276b99c0ce987ac060f6439d6334e5f17492
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-09 03:11:23 +0800

    Register Macro and Observer Service Provider

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\MacroServiceProvider::class,
+        App\Providers\ObserverServiceProvider::class,
 
     ],
 
