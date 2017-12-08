555ecd9a808eb728ac88f6a03903c28613293842
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-09 03:10:58 +0800

    Added Description to Logs Activity

--- app/Traits/LogsActivityExtended.php
+++ app/Traits/LogsActivityExtended.php
@@ -16,8 +16,23 @@ trait LogsActivityExtended
      */
     protected static $logOnlyDirty = true;
 
+    /**
+     * Get Log Name to Use
+     * @param  string $eventName
+     * @return string
+     */
     public function getLogNameToUse(string $eventName = ''): string
     {
         return str_slug_fqcn($this);
+    }
+
+    /**
+     * Set Standard Description
+     * @param  string $eventName
+     * @return string
+     */
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "This " . strtolower(class_basename($this)) . " has been {$eventName}";
     }
 }
