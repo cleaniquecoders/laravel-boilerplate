11b459abc7e59ddee0bf4eaea78c4fcbaed5a474
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-09 01:56:16 +0800

    Added Table Component

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,12 @@
+<table class="table table-condensed table-striped table-hover"
+	id="{{ $table_id or 'table-id' }}">
+	<thead>
+		{{ $thead ?? '' }}
+	</thead>
+	<tbody>
+		{{ $tbody ?? '' }}
+	</tbody>
+	<tfoot>
+		{{ $tfoot ?? '' }}
+	</tfoot>
+</table>
