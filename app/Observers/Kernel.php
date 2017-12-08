a9c929e712be08c540b06a41d411b96bf18be707
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-09 02:46:17 +0800

    Added Observer Kernel

---
+++ app/Observers/Kernel.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Observers;
+
+/**
+ *
+ */
+class Kernel
+{
+    /**
+     * Single one-on-one observer
+     * @var array
+     */
+    protected $observers = [
+        // FQCN of Model => FQCN of Observer
+    ];
+
+    /**
+     * One observer observed by many models
+     * @var array
+     */
+    protected $observeBy = [
+        \App\Observers\ReferenceObserver::class => [
+
+        ],
+        \App\Observers\HashidsObserver::class   => [
+            \App\Models\User::class,
+        ],
+    ];
+
+    /**
+     * Make this class
+     * @return \NIBM\Observers\Kernel
+     */
+    public static function make()
+    {
+        return (new self);
+    }
+
+    /**
+     * Register observers
+     * @return void
+     */
+    public function observes()
+    {
+        $this->observeSingle();
+        $this->observeBy();
+    }
+
+    /**
+     * Observe One-on-One Model-Observer
+     * @return void
+     */
+    private function observeSingle()
+    {
+        if (count($this->observers) > 0) {
+            foreach ($this->observers as $model => $observer) {
+                if (class_exists($model) && class_exists($observer)) {
+                    $model::observe($observer);
+                }
+            }
+        }
+    }
+
+    /**
+     * One observer observed by many models
+     * @return void
+     */
+    private function observeBy()
+    {
+        if (count($this->observeBy) > 0) {
+            foreach ($this->observeBy as $observer => $models) {
+                foreach ($models as $model) {
+                    if (class_exists($model) && class_exists($observer)) {
+                        $model::observe($observer);
+                    }
+                }
+            }
+        }
+    }
+}
