828f5982c84c46816518f59984746061ba90b97a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-10 07:52:57 +0800

    Added Upload Avatar Form

--- resources/views/users/avatar.blade.php
+++ resources/views/users/avatar.blade.php
@@ -1,11 +1,9 @@
 @component('components.panels.small', ['panel_title' => 'Avatar'])
     @slot('panel_body')
-        @if(auth()->user()->getFirstMediaUrl('avatar'))
-
-        @endif
-		{{ html()->form('POST', route('store.avatar'))->open() }}
-			{{ html()->label('Plase choose your avatar') }}
-			{{ html()->input('file', 'avatar') }}
+		{{ html()->form('POST', route('store.avatar'))->attribute('enctype', 'multipart/form-data')->open() }}
+			{{ html()->label('Please choose your avatar') }}
+			{{ html()->input('file')->name('avatar') }}
+			{{ html()->button('Submit', 'submit')->class('btn btn-sm btn-default') }}
 		{{ html()->form()->close() }}
     @endslot
 @endcomponent
