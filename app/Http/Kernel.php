25cf0b96208f75612f5c52f543decb0b5edbde37
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-10 07:14:28 +0800

    Added Sweet Alert

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Spatie\Referer\CaptureReferer::class,
+            \UxWeb\SweetAlert\ConvertMessagesIntoSweetAlert::class,
         ],
 
         'api' => [
