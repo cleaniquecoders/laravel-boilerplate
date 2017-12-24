b78edcd20a99f39948d949f0d3112cd299a0f423
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-24 16:47:53 +0800

    Fixed Misspelled Phone Type Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(CountrySeeder::class);
-        $this->call(PhoneType::class);
+        $this->call(PhoneTypeSeeder::class);
     }
 }
