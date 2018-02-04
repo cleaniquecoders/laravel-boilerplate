74381b1f59db52e35e7e2cacd4105b699e161502
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-04 09:45:28 +0800

    Added reload:db command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\ReloadDbCommand::class,
     ];
 
     /**
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
