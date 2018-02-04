b3ce911f744bd0b3313224cb6f53ee5cd6054658
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-04 09:26:11 +0800

    Refactor load dev providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,14 +7,31 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * List of providers for development
+     * @var array
+     */
+    protected $providers_dev = [];
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
     {
+        $this->bootDevProviders();
+    }
+
+    /**
+     * Boot Development Providers
+     * @return void
+     */
+    private function bootDevProviders()
+    {
         if ($this->app->environment() !== 'production') {
-            $this->app->register(\NasrulHazim\ArtisanMakers\ArtisanMakersServiceProvider::class);
+            foreach ($this->providers_dev as $provider) {
+                $this->app->register($provider);
+            }
         }
     }
 
