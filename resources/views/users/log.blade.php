01d7a5ba4f6059282b078fd9f1f00f2041d85070
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-04 22:21:26 +0800

    Refactor UI

--- resources/views/users/log.blade.php
+++ resources/views/users/log.blade.php
@@ -3,24 +3,26 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-md-center mt-5">
-            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+            <div class="col">
                 @component('components.card', ['card_title' => 'Avatar'])
-                    @component('components.table')
-                        @slot('thead')
-                            <tr>
-                                <th>Description</th>
-                                <th>Day, Date &amp; Time</th>
-                            </tr>
-                        @endslot
-                        @slot('tbody')
-                            @foreach($logs as $log)
+                    @slot('card_body')
+                        @component('components.table')
+                            @slot('thead')
                                 <tr>
-                                    <td>{{ $log->description }}</td>
-                                    <td>{{ $log->created_at->toDayDateTimeString() }}</td>
+                                    <th>Description</th>
+                                    <th>Day, Date &amp; Time</th>
                                 </tr>
-                            @endforeach
-                        @endslot
-                    @endcomponent
+                            @endslot
+                            @slot('tbody')
+                                @foreach($logs as $log)
+                                    <tr>
+                                        <td>{{ $log->description }}</td>
+                                        <td>{{ $log->created_at->toDayDateTimeString() }}</td>
+                                    </tr>
+                                @endforeach
+                            @endslot
+                        @endcomponent
+                    @endslot
                 @endcomponent
             </div>
         </div>
