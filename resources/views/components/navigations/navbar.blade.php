31bf7fbd35f5e2215cd7e82a08937b88bdf22e5b
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-06 15:22:53 +0800

    FIxed register link

--- resources/views/components/navigations/navbar.blade.php
+++ resources/views/components/navigations/navbar.blade.php
@@ -14,7 +14,7 @@
       <a class="nav-link" href="{{ route('login') }}">Login</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" {{ route('register') }}">Register</a>
+      <a class="nav-link" href="{{ route('register') }}">Register</a>
     </li>
     @else
       <li class="nav-item">
