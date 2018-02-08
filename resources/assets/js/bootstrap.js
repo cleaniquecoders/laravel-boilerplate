ae59c8acead9269340fee629062f8d75a6d0a08a
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-09 06:41:21 +0800

    Update popper.js setup.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,8 +1,6 @@
 
-
 window._ = require('lodash');
-
-window.Popper = require('popper.js/dist/umd/popper');
+window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
