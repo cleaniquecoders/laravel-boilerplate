d14bf73d8749cc5e2530323775074324dce11af8
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-16 16:33:29 +0800

    Added Font-Awesome

--- resources/assets/js/font-awesome.js
+++ resources/assets/js/font-awesome.js
@@ -2,10 +2,10 @@
  * Font Awesome 5
  */
 var fontawesome = require('@fortawesome/fontawesome');
-// var solid       = require('@fortawesome/fontawesome-free-solid').default;
-var regular     = require('@fortawesome/fontawesome-free-regular').default;
+var solid       = require('@fortawesome/fontawesome-free-solid').default;
+var regular     = require('@fortawesome/fontawesome-free-regular');
 // var brands      = require('@fortawesome/fontawesome-free-brands');
 
-// fontawesome.library.add(solid);
+fontawesome.library.add(solid);
 fontawesome.library.add(regular);
 // fontawesome.library.add(brands);
