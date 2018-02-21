407a7a041b95e33461096b9d7eae32b743f34ff8
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-02-21 15:01:28 +0800

    Added Tab Component

---
+++ resources/views/components/tab/navigation.blade.php
@@ -0,0 +1,6 @@
+<a class="list-group-item list-group-item-action {{ $navigation['is_active'] ? 'show active' : '' }}"
+	data-toggle="list"
+	href="#{{ $navigation['content_id'] }}"
+	role="tab">
+	{{ $navigation['label'] }}
+</a>
