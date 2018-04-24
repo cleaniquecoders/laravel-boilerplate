e0da637d564a48570ace2497442c8a147925a5fb
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-24 13:42:54 +0800

    Update ACL Setup to have developer roles, added permissions and actions.

--- config/acl.php
+++ config/acl.php
@@ -2,7 +2,19 @@
 
 return [
     'roles' => [
+    	'developer',
         'administrator',
         'user',
+    ],
+    'permissions' => [
+    	'setting' => ['developer', 'administrator'],
+    	'passport' => ['developer'],
+    	'user' => ['developer', 'administrator']
+    ],
+    'actions' => [
+    	'index', 'show', 
+    	'create', 'store', 
+    	'edit', 'update', 
+    	'destroy'
     ],
 ];
