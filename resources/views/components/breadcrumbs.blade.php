b0bf3193abe7ab94ace2d9e37e2e5a69129faf5b
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-24 16:40:09 +0800

    Customise Breadcrumb

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,15 @@
+@if (count($breadcrumbs))
+
+    <ol class="breadcrumb bg-transparent rounded-0 border-bottom border-secondary">
+        @foreach ($breadcrumbs as $breadcrumb)
+
+            @if ($breadcrumb->url && !$loop->last)
+                <li class="breadcrumb-item"><a class="text-primary" href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+            @else
+                <li class="breadcrumb-item text-dark active">{{ $breadcrumb->title }}</li>
+            @endif
+
+        @endforeach
+    </ol>
+
+@endif
