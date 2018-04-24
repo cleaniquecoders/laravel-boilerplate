0a0a2a7a8a536df2714a00a173f854b3d66e35c0
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-24 18:10:34 +0800

    Refactor Observer Setup

--- app/Observers/Kernel.php
+++ app/Observers/Kernel.php
@@ -5,29 +5,6 @@ namespace App\Observers;
 class Kernel
 {
     /**
-     * Single one-on-one observer.
-     *
-     * @var array
-     */
-    protected $observers = [
-        // FQCN of Model => FQCN of Observer
-    ];
-
-    /**
-     * One observer observed by many models.
-     *
-     * @var array
-     */
-    protected $observeBy = [
-        \App\Observers\ReferenceObserver::class => [
-        ],
-        \App\Observers\HashidsObserver::class => [
-            \App\Models\User::class,
-            \Spatie\MediaLibrary\Media::class,
-        ],
-    ];
-
-    /**
      * Make this class.
      *
      * @return \App\Observers\Kernel
@@ -42,35 +19,10 @@ class Kernel
      */
     public function observes()
     {
-        $this->observeSingle();
-        $this->observeBy();
-    }
-
-    /**
-     * Observe One-on-One Model-Observer.
-     */
-    private function observeSingle()
-    {
-        if (count($this->observers) > 0) {
-            foreach ($this->observers as $model => $observer) {
+        foreach (config('observers') as $observer => $models) {
+            foreach ($models as $model) {
                 if (class_exists($model) && class_exists($observer)) {
                     $model::observe($observer);
-                }
-            }
-        }
-    }
-
-    /**
-     * One observer observed by many models.
-     */
-    private function observeBy()
-    {
-        if (count($this->observeBy) > 0) {
-            foreach ($this->observeBy as $observer => $models) {
-                foreach ($models as $model) {
-                    if (class_exists($model) && class_exists($observer)) {
-                        $model::observe($observer);
-                    }
                 }
             }
         }
