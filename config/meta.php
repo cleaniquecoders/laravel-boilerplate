5b41e0fe643183f9a644a176e61300ad715fdd0c
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-24 15:28:17 +0800

    Added meta config & component.

---
+++ config/meta.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'author'      => env('META_AUTHOR', env('APP_URL')),
+    'keywords'    => env('META_KEYWORDS', 'Laravel, Boileplate, Bootstrap, Axios, VueJs, Datatable, SweetAlert2, Select2'),
+    'description' => env('META_DESCRIPTION', 'Laravel Boileplate to Simplify Your Web Application Development'),
+    'title'       => env('META_TITLE', env('APP_URL')),
+];
