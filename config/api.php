233b04e65fb4464632a17f322765f5fb94639e82
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-24 15:30:05 +0800

    Added API Config

---
+++ config/api.php
@@ -0,0 +1,9 @@
+<?php 
+
+return [
+	'header' => [
+		'prefix' => env('API_HEADER_PREFIX', 'CleaniqueCoders'),
+		'accept' => 'application/vnd.' . env('API_HEADER_PREFIX', 'CleaniqueCoders') . '.' . env('API_VERSION', 'v1') . '+json'
+	],
+	'version' => env('API_VERSION', 'v1'),
+];
