f49e86bbf34a672341b19f7ab207a7fbf2bc06b2
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-25 19:14:02 +0800

    Update modal's button component

--- resources/views/components/modals/button.blade.php
+++ resources/views/components/modals/button.blade.php
@@ -1,10 +1,6 @@
-<button type="button" class="btn btn-sm btn-primary" @include('components.tooltip', ['tooltip' => $tooltip])
+<button type="button" class="btn btn-sm btn-primary" 
+	@include('components.tooltip', ['tooltip' => __($label)])
   data-toggle="modal" data-target="#{{ $id }}">
-  @isset($icon)
-  <i class="{{ $icon }}"></i>
-  @endisset
-
-  @isset($label)
-	{{ __($label) }}
-  @endisset
+  @isset($icon) <i class="{{ $icon }}"></i> @endisset
+  @isset($label) {{ __($label) }} @endisset
 </button>
