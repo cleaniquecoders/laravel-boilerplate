c29bd69d01cfc61bc7fa36864283c27f5cf676e9
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-04-25 19:14:21 +0800

    Make use of @isset directive for table component

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,13 +1,23 @@
 <div class="table-responsive">
-	<table class="table {{ $table_classes or 'table-sm table-transparent table-hover' }}" id="{{ $table_id or 'table-id' }}">
-		<thead>
-			{{ $thead ?? '' }}
-		</thead>
-		<tbody>
-			{{ $tbody ?? '' }}
-		</tbody>
-		<tfoot>
-			{{ $tfoot ?? '' }}
-		</tfoot>
+	<table 
+		class="table {{ $table_classes or 'table-sm table-transparent table-hover' }}" 
+		id="{{ $table_id or 'table-id' }}">
+		@isset($thead)
+			<thead>
+				{{ $thead ?? '' }}
+			</thead>
+		@endisset
+
+		@isset($thead)
+			<tbody>
+				{{ $tbody ?? '' }}
+			</tbody>
+		@endisset
+
+		@isset($thead)
+			<tfoot>
+				{{ $tfoot ?? '' }}
+			</tfoot>
+		@endisset
 	</table>
 </div>
