870a5e6770da2f485c8b306556f19459b730afa4
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-03 04:18:40 +0800

    Added config file

---
+++ config/theme.php
@@ -0,0 +1,16 @@
+<?php 
+
+return [
+	/**
+	 * Set default theme. Default is Tabler.
+	 */
+	'default' => env('THEME_DEFAULT', 'tabler'),
+
+	/**
+	 * Available Themes in the application
+	 * This can be use for switchable theme feature.
+	 */
+	'themes' => [
+		'tabler'
+	]
+];
