8320347d6f03a1ffaff9733bb7a084795004c7ee
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-07 22:15:59 +0800

    Added Tabler scripts.

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,6 +1,16 @@
-<!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ theme('url', 'assets/js/require.min.js', 'tabler') }}"></script>
+<script>
+  requirejs.config({
+      baseUrl: '{{ config('app.url') }}/tabler'
+  });
+</script>
 <script src="{{ asset('js/font-awesome.js') }}"></script>
 @include('vendor.sweetalert.view')
 @routes
 @translations
+<script>
+    jQuery(document).ready(function($) {
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
