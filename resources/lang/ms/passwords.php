609b156a44136144599ff776e02c5f4cf0fb4fa9
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-07 23:14:40 +0800

    Added Language ms.

---
+++ resources/lang/ms/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+   
+    'password' => 'Katalaluan hendaklah sekurang-kurangnya enam aksara dan mesti sama dengan katalaluan pengesahan.',
+    'reset'    => 'Katalaluan telah ditetapkan semula.',
+    'sent'     => 'Kami telah menghantar emel untuk tetapan katalaluan.',
+    'token'    => 'Token tetapan semula katalaluan tidak sah.',
+    'user'     => "Kami tidak menjumpai pengguna dengan emel yang diberikan.",
+];
