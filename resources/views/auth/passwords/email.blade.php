3596486a5e8ae268d2ba4eceb171e9ca53876408
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-07 22:15:19 +0800

    Refactor Auth views to use Tabler

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,31 @@
-@extends('layouts.app')
+@component('layouts.auth')
+	@slot('auth_content')
+		<form class="card" method="POST" action="{{ route('password.email') }}">
+			@csrf
+			<div class="card-body p-6">
+				<div class="card-title text-center">Forgot Password</div>
+				<hr>
+				@if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+				<div class="form-group">
+					<label for="email" class="form-label">E-Mail Address</label>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Reset Password</div>
+					<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+					@if ($errors->has('email'))
+						<span class="invalid-feedback">
+							<strong>{{ $errors->first('email') }}</strong>
+						</span>
+					@endif
+				</div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+				<div class="form-footer">
+					<button type="submit" class="btn btn-primary btn-block">Send Password Reset Link</button>
+				</div>
+			</div>
+		</form>
+	@endslot
+@endcomponent
