7681533978cf9911580692d0b79db099bc2cb240
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-15 23:19:12 +0800

    Disable Table JS

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,15 +1,16 @@
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ theme('url', 'assets/js/require.min.js', 'tabler') }}"></script>
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="{{ asset('js/axios.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+{{-- <script src="{{ theme('url', 'assets/js/require.min.js', 'tabler') }}"></script>
 <script src="{{ theme('url', 'assets/js/core.js', 'tabler') }}"></script>
 <script>
   requirejs.config({
       baseUrl: '{{ config('app.url') }}/tabler'
   });
-</script>
+</script> --}}
 <script src="{{ asset('js/font-awesome.js') }}"></script>
 @include('vendor.sweetalert.view')
 @routes
-@translations
 @stack('scripts')
 <script>
     jQuery(document).ready(function($) {
