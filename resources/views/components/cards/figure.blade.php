c0441333863b82059f33c5471a597746850f9fd1
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-15 11:34:01 +0800

    Added Component Card for Figure

---
+++ resources/views/components/cards/figure.blade.php
@@ -0,0 +1,14 @@
+<div class="card">
+	<div class="card-body p-3 text-center">
+		<div class="text-right text-green">
+			{{ $header }}
+			@icon($icon)
+		</div>
+		<div class="h1 m-0">
+			{{ $content }}
+		</div>
+		<div class="text-muted mb-4">
+			{{ $footer }}
+		</div>
+	</div>
+</div>
