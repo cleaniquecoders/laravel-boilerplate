46e513f4c9bcea284c38210fafe8c2e5c765a30e
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-05-15 11:34:44 +0800

    Use card figure for date.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,12 @@
 @section('content')
 <div class="row">
 	<div class="col-6 col-sm-4 col-lg-2">
-	<div class="card">
-		<div class="card-body p-3 text-center">
-			<div class="text-right text-green">
-			{{ date('Y') }}
-			<i class="fe fe-calendar"></i>
-			</div>
-			<div class="h1 m-0">{{ date('d') }}</div>
-			<div class="text-muted mb-4">{{ date('M') }}</div>
-		</div>
-		</div>
+		@include('components.cards.figure', [
+				'header' => date('Y'),
+				'icon' => 'fe fe-calendar',
+				'content' => date('d'),
+				'footer' => date('M')
+			])
 	</div>
 </div>
 @endsection
