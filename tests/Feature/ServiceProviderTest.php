52fc6344b99103a40a6019afa5996f2d3f952ad5
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-09-08 19:00:57 +0800

    Remove Service Provider Test for Packages

--- tests/Feature/ServiceProviderTest.php
+++ tests/Feature/ServiceProviderTest.php
@@ -19,47 +19,4 @@ class ServiceProviderTest extends TestCase
             $this->assertTrue(in_array($provider, $config));
         }
     }
-
-    /** @test */
-    public function it_has_cleanique_coders_packages()
-    {
-        $config    = config('app.providers');
-        $providers = [
-            \CleaniqueCoders\Blueprint\Macro\BlueprintMacroServiceProvider::class,
-            \CleaniqueCoders\Profile\ProfileServiceProvider::class,
-            \CleaniqueCoders\ArtisanMakers\ArtisanMakersServiceProvider::class,
-        ];
-        foreach ($providers as $provider) {
-            $this->assertTrue(in_array($provider, $config));
-        }
-    }
-
-    /** @test */
-    public function it_has_spatie_packages()
-    {
-        $config    = config('app.providers');
-        $providers = [
-            \Spatie\Permission\PermissionServiceProvider::class,
-            \Spatie\Analytics\AnalyticsServiceProvider::class,
-            \Spatie\Menu\Laravel\MenuServiceProvider::class,
-            \Spatie\GoogleCalendar\GoogleCalendarServiceProvider::class,
-            \Spatie\Html\HtmlServiceProvider::class,
-        ];
-        foreach ($providers as $provider) {
-            $this->assertTrue(in_array($provider, $config));
-        }
-    }
-
-    /** @test */
-    public function it_has_other_packages()
-    {
-        $config    = config('app.providers');
-        $providers = [
-            \Softon\SweetAlert\SweetAlertServiceProvider::class,
-            \DaveJamesMiller\Breadcrumbs\BreadcrumbsServiceProvider::class,
-        ];
-        foreach ($providers as $provider) {
-            $this->assertTrue(in_array($provider, $config));
-        }
-    }
 }
